26eda22509bec714c5c270ddda82b5a3e5ea703f
Balaji7259 <balaji.techy22@gmail.com>
2025-07-13 16:54:18 +0530

    GROUP BY and ORDER BY

---
+++ 4.GROUP BY and ORDER BY.sql
@@ -0,0 +1,29 @@
+-- GROUP By
+Use parks_and_recreation;
+Select * From
+employee_demographics;
+
+-- Select gender 
+-- from employee_demographics
+-- group by gender;
+
+Select gender, AVG(age), max(age)
+from employee_demographics
+group by gender;
+
+select * From
+employee_salary;
+
+select occupation,salary
+from employee_salary
+group by occupation,salary;
+
+
+-- ORDER BY
+SELECT *
+FROM employee_demographics
+ORDER BY first_name desc
+;
+
+SELECT * FROM employee_demographics
+order by gender,age;
